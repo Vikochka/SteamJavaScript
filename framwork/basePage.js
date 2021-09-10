dfc6f3d798978be17e6a67677d30bd53eb0f953e
Viktoriya <62216160+Vikochka@users.noreply.github.com>
2021-09-10 12:53:45 +0300

    Delete framwork directory

--- framwork/basePage.js
+++
@@ -1,8 +0,0 @@
-const setting = require("../setting.json");
-
-module.exports = class BasePage {
-    async open() {
-        await browser.setTimeout({'pageLoad': JSON.stringify(setting.timeout)})
-        return browser.url(JSON.stringify(setting.URL));
-    }
-}
